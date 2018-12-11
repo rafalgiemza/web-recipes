d92579a7f2c126ada30e7e056400c5356a21beaa
Rafał Giemza <rafal.giemza@intive.com>
2018-12-11 10:17:23 +0100

    google maps example

---
+++ pages/7_js_google_map_example/main.js
@@ -0,0 +1,3 @@
+// The location of Uluru
+const office = { lat: 51.102460, lng: 17.032210 };
+const officeUrl = "https://www.google.com/maps/place/intive/@51.1024441,17.0300821,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x9a732b8c6677286e!8m2!3d51.1024407!4d17.0322713";
